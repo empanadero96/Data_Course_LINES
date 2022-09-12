71940c0fccbaa55d991240b09c9e8b53ad417872
Preston <empanadero96@gmail.com>
2022-09-11 23:21:17 -0600

    Trying again

---
+++ functions we know.R
@@ -0,0 +1,25 @@
+
+readLines
+list.files
+reas.csv
+c()
+length()
+head()
+for(car in vectorstuff){}
+plot()
+
+
+plot(x=df$mass,y=df$wingspan)
+
+sum(df$wingspan)
+mean(df$wingspan)
+median(df$wingspan)
+sd(df$wingspan)
+max(df$wingspan)
+min(df$wingspan)
+
+summary(df$wingspan)
+median(c(1:10))
+
+df
+
